0f8a79f29c6d1a59332e3823454f6bd753dfb671
omarYasirR <omaryasir5712@gmail.com>
2025-08-09 20:48:09 +0200

    adding key prop

--- pages/features.js
+++ pages/features.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import MainComp from '../Components/mainComp'
+import React from "react";
+import MainComp from "../Components/mainComp";
 import Home from "../styles/Home.module.css";
 
-import styles from '../styles/Features.module.css'
-import Testimonial from '../Components/Testimonial';
-import Head from 'next/head';
+import styles from "../styles/Features.module.css";
+import Testimonial from "../Components/Testimonial";
+import Head from "next/head";
 import { MdNotificationsActive } from "react-icons/md";
 import { SiGooglemessages } from "react-icons/si";
 import { IoLayers } from "react-icons/io5";
@@ -14,42 +14,40 @@ import { FaFileArchive } from "react-icons/fa";
 import { SiGuilded } from "react-icons/si";
 import { FaShoppingCart } from "react-icons/fa";
 
-
-
 const features = () => {
   const data = [
-    {icon: <MdNotificationsActive />, h3: 'Notificatons'},
-    {icon: <SiGooglemessages />, h3: 'Messeging'},
-    {icon: <IoLayers />, h3: 'Layers'},
-    {icon: <FaHourglassStart />, h3: 'Startup'},
-    {icon: <HiMiniPresentationChartBar />, h3: 'Presentaton'},
-    {icon: <FaFileArchive />, h3: 'Archiving'},
-    {icon: <SiGuilded />, h3: 'Guidelines'},
-    {icon: <FaShoppingCart />, h3: 'Shope'}
-  ]
+    { icon: <MdNotificationsActive />, h3: "Notificatons" },
+    { icon: <SiGooglemessages />, h3: "Messeging" },
+    { icon: <IoLayers />, h3: "Layers" },
+    { icon: <FaHourglassStart />, h3: "Startup" },
+    { icon: <HiMiniPresentationChartBar />, h3: "Presentaton" },
+    { icon: <FaFileArchive />, h3: "Archiving" },
+    { icon: <SiGuilded />, h3: "Guidelines" },
+    { icon: <FaShoppingCart />, h3: "Shope" },
+  ];
   return (
     <div>
       <Head>
         <title>Features</title>
       </Head>
-      <MainComp head={'Features'} />
+      <MainComp head={"Features"} />
       <div className="container">
         <div className={styles.feat_cont}>
-          {data.map(item => (
-            <div className={Home.box}>
-            <div className={Home.icon}>{item.icon}</div>
-            <h3>{item.h3}</h3>
-            <p className={Home.p}>
-              Far far away, behind the word mountains, far from the countries
-              Vokalia and Consonantia, there live the blind texts.
-            </p>
-          </div>
+          {data.map((item, i) => (
+            <div className={Home.box} key={i}>
+              <div className={Home.icon}>{item.icon}</div>
+              <h3>{item.h3}</h3>
+              <p className={Home.p}>
+                Far far away, behind the word mountains, far from the countries
+                Vokalia and Consonantia, there live the blind texts.
+              </p>
+            </div>
           ))}
         </div>
       </div>
       <Testimonial />
     </div>
-  )
-}
+  );
+};
 
-export default features
+export default features;
--- pages/pricing.js
+++ pages/pricing.js
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import React from 'react'
-import Price from '../Components/price'
-import Talk from '../Components/Talk';
-import features from '../styles/Features.module.css'
+import Head from "next/head";
+import React from "react";
+import Price from "../Components/price";
+import Talk from "../Components/Talk";
+import features from "../styles/Features.module.css";
 import Home from "../styles/Home.module.css";
 import { FaInbox } from "react-icons/fa";
 import { BsFillDisplayFill } from "react-icons/bs";
@@ -13,22 +13,17 @@ import { RiGuideFill } from "react-icons/ri";
 import { SiShopee } from "react-icons/si";
 import { RiGalleryFill } from "react-icons/ri";
 
-
-
-
-
-
 const Pricing = () => {
   const data = [
-    {icon: <FaInbox /> , h3: 'Out of The Box'},
-    {icon: <BsFillDisplayFill />, h3: 'Great Display'},
-    {icon: <FaVideo /> , h3: 'Video'},
-    {icon: <RiGalleryFill />, h3: 'Gallery'},
-    {icon: <HiMiniPresentationChartLine /> , h3: 'Presentaton'},
-    {icon: <FaFileArchive /> , h3: 'Archiving'},
-    {icon: <RiGuideFill /> , h3: 'Guidelines'},
-    {icon: <SiShopee />, h3: 'Shope'}
-  ]
+    { icon: <FaInbox />, h3: "Out of The Box" },
+    { icon: <BsFillDisplayFill />, h3: "Great Display" },
+    { icon: <FaVideo />, h3: "Video" },
+    { icon: <RiGalleryFill />, h3: "Gallery" },
+    { icon: <HiMiniPresentationChartLine />, h3: "Presentaton" },
+    { icon: <FaFileArchive />, h3: "Archiving" },
+    { icon: <RiGuideFill />, h3: "Guidelines" },
+    { icon: <SiShopee />, h3: "Shope" },
+  ];
   return (
     <div>
       <Head>
@@ -38,24 +33,28 @@ const Pricing = () => {
       <div className="container">
         <div className="heading">
           <h3>more features</h3>
-          <h2>Far far away, behind the word mountains, far from the countries</h2>
+          <h2>
+            Far far away, behind the word mountains, far from the countries
+          </h2>
         </div>
         <div className={features.feat_cont}>
-          {data.map(item => (
-            <div className={Home.box} style={{textAlign: 'center'}}>
-            <div className={Home.icon} style={{alignSelf: 'center'}}>{item.icon}</div>
-            <h3>{item.h3}</h3>
-            <p className={Home.p}>
-              Far far away, behind the word mountains, far from the countries
-              Vokalia and Consonantia, there live the blind texts.
-            </p>
-          </div>
+          {data.map((item, i) => (
+            <div className={Home.box} key={i} style={{ textAlign: "center" }}>
+              <div className={Home.icon} style={{ alignSelf: "center" }}>
+                {item.icon}
+              </div>
+              <h3>{item.h3}</h3>
+              <p className={Home.p}>
+                Far far away, behind the word mountains, far from the countries
+                Vokalia and Consonantia, there live the blind texts.
+              </p>
+            </div>
           ))}
         </div>
       </div>
       <Talk />
     </div>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
